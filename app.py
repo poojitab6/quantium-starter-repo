01d40ea8eb67e2beece61ba6b5a7b7e5567e5726
Poojita Bondalapati <pooja.bondalapati@gmail.com>
2026-04-13 18:37:23 +0100

    Changed from dropdown to radio buttons.

--- app.py
+++ app.py
@@ -22,10 +22,17 @@ with open(doc, 'r') as file:
         region.append(row['region'])
 
 df = pd.DataFrame({
-    "Sales": pd.to_numeric(sales),
-    "Date": pd.to_datetime(date),
+    "Sales": sales,
+    "Date": date,
     "Region": region
 })
+
+df['Region'] = df['Region'].str.lower()
+df['Sales'] = pd.to_numeric(df['Sales'])
+df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
+
+df = df.sort_values(by='Date')
+
 
 fig = px.line(df, x="Date", y="Sales", color="Region", labels={"Date": "Date", "Sales": "Sales ($)", "Region": "Region"})
 
@@ -42,14 +49,17 @@ app.layout = html.Div([
         html.Div([
             html.H2("Pink Morsel Sales by Region"),
 
-            dcc.Dropdown(
+            dcc.RadioItems(
                 id='region-dropdown',
                 options = [
-                    {'label': r, 'value': r}
-                    for r in df['Region'].unique()
+                    {'label':'North', 'value':'north'},
+                    {'label':'East', 'value':'east'},
+                    {'label':'South', 'value':'south'},
+                    {'label':'West', 'value':'west'},
+                    {'label':'All', 'value':'all'}
                 ],
-                value=df['Region'].unique()[0], #default
-                clearable=False
+                value='all', #default
+                inline=True
             ),
 
             dcc.Graph(id='line-graph')
@@ -58,19 +68,27 @@ app.layout = html.Div([
 
 ])
 
-if __name__== '__main__':
-    app.run(debug=True)
-
 @app.callback(
     Output('line-graph', 'figure'),
     Input('region-dropdown', 'value')
 )
 
 def update_graph(selected_region):
-    filtered_df = df[df['Region'] == selected_region]
+
+    if selected_region == 'all':
+        filtered_df = df
+        title = "Sales in All Regions"
+        color = "Region"
+    else:
+        filtered_df = df[df['Region'] == selected_region]
+        title = f"Sales in {selected_region.capitalize()}"
+        color = None
 
     fig = px.line(
-        filtered_df, x="Date", y="Sales", title=f"Sales in {selected_region}"
+        filtered_df, x="Date", y="Sales", color=color, title=title
     )
 
     return fig
+
+if __name__== '__main__':
+    app.run(debug=True)
